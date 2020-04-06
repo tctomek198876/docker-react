9ca1dbe1f12a1accb73f80da085802e97c265a04
tctomek198876 <58735228+tctomek198876@users.noreply.github.com>
2020-04-06 17:17:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Tomek!!!</h1>
         </header>
         <p className="App-intro">
           I was changed on the Feature branch
